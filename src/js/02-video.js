0ba3dd297def662e2484ccd0f450998f4f1aef53
Yevhen Kontselenko <105706808+YevhenKon@users.noreply.github.com>
2023-03-22 21:49:54 +0200

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle')
+
+const iframe = document.querySelector('#vimeo-player');
+    const player = new Player(iframe);
+
+    player.on('timeupdate', throttle(function(evt) {
+        // console.log(seconds);
+        localStorage.setItem("videoplayer-current-time", JSON.stringify(evt.seconds));
+        
+        
+        
+    }, 1000));
+
+
+    const saveTime = localStorage.getItem("videoplayer-current-time")
+const stopVideo = JSON.parse(saveTime) || 0
+    
+player.setCurrentTime(stopVideo)
+
 
