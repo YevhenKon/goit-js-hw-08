e1870d3a4cd442b599bb73287e3fded0051dc84e
Yevhen Kontselenko <105706808+YevhenKon@users.noreply.github.com>
2023-03-24 20:41:33 +0200

    fix 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,47 +1,52 @@
 import throttle from "lodash.throttle";
 
+ const localhostKey = "feedback-form-state"
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    input: document.querySelector('input'),
+}
+
 const inputInformation = {
     email: '',
     message: '',
 }
 
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    input: document.querySelector('input'),
-    textarea: document.querySelector('.feedback-form textarea')
-}
-
-refs.form.addEventListener('input', throttle(onTextareaInput, 500))
 refs.form.addEventListener('submit', onFormSubmit)
-
-function onTextareaInput(evt) {
-    if (evt.target === refs.input) {
-        inputInformation.email = evt.target.value
-        const targetValue = JSON.stringify(inputInformation)
-        localStorage.setItem("feedback-form-state", targetValue)
-    }
-    if (evt.target === refs.textarea) {
-        inputInformation.message = evt.target.value
-        const targetValue = JSON.stringify(inputInformation)
-        localStorage.setItem("feedback-form-state", targetValue)
-    }
-}
 
 inputSavedText()
 
 function onFormSubmit(evt) {
     evt.preventDefault()
 
-    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+    console.log(JSON.parse(localStorage.getItem(localhostKey)));
     evt.currentTarget.reset()
-    localStorage.removeItem("feedback-form-state")
+    localStorage.removeItem(localhostKey)
+}
+
+refs.form.addEventListener('input', throttle(onTextareaInput, 500))
+
+function onTextareaInput(evt) {
+
+    inputInformation[evt.target.name] = evt.target.value
+
+    const targetValue = JSON.stringify(inputInformation)
+    localStorage.setItem(localhostKey, targetValue)
 }
  
 function inputSavedText() {
-    const savedMessage = localStorage.getItem("feedback-form-state")
+    const savedMessage = localStorage.getItem(localhostKey)
     if (savedMessage) {
+        console.log(savedMessage);
         const infoObj = JSON.parse(savedMessage)
-        refs.input.value = infoObj.email;
-        refs.textarea.value = infoObj.massage;
+        refs.textarea.value = infoObj.massage || '';
+        refs.input.value = infoObj.email || '';
     }
 }
+
+console.log(inputInformation);
+
+
+
+
